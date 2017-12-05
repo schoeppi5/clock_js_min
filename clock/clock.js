e2c537082d45385d2a7f77df4202044e7c2aaee5
schoeppi5 <whatkarl@gmail.com>
2017-12-05 17:11:24 +0100

    All the files neccessarry for the clock to work

---
+++ clock/clock.js
@@ -0,0 +1,105 @@
+$('document').ready(function()
+{
+	radius = resize();
+	clock(radius);
+	setInterval(function(){clock(radius)}, 1);
+	
+	$(window).resize(function()
+	{
+		radius = resize();
+	});
+});
+
+function clock (r)
+{
+	radius = r;
+	lengthS = 8 * (radius / 10);
+	lengthM = 7 * (radius / 10);
+	lengthMI = 9 * (radius / 10);
+	lengthH = 6 * (radius / 10);
+	
+	var currDate = new Date();
+	var minutes = currDate.getMinutes();
+	var hours = currDate.getHours() % 12;
+	var seconds = currDate.getSeconds();
+	var milli = currDate.getMilliseconds();
+	
+	var angleMI = milli * (360/1000);
+	var milliX = Math.sin(toRadians(angleMI)) * lengthMI + radius;
+	var milliY = Math.cos(toRadians(angleMI)) * lengthMI + radius;
+	milliY = radius - (milliY - radius);
+	var angleS = seconds * (360/60);
+	var secondX = Math.sin(toRadians(angleS)) * lengthS + radius;
+	var secondY = Math.cos(toRadians(angleS)) * lengthS + radius;
+	secondY = radius - (secondY - radius);
+	var angleM = minutes * (360/60);								//Winkel des Minutenzeigers
+	var minuteX = Math.sin(toRadians(angleM)) * lengthM + radius;	//X-Koordinate anhand des Winkels zur gedachten Y-Achse (o(50/50))
+	var minuteY = Math.cos(toRadians(angleM)) * lengthM + radius;  	//Y-Koordinate anhand des Winkels zur gedachten Y-Achse (o(50/50))
+	minuteY = radius - (minuteY - radius); 							//Spiegeln, da Y-Achse von oben nach unten geht
+	var angleH = (hours * (360/12)) + (minutes * (30/60));
+	var hourX = Math.sin(toRadians(angleH)) * lengthH + radius;		//X-Koordinate anhand des Winkels zur gedachten Y-Achse (o(50/50))
+	var hourY = Math.cos(toRadians(angleH)) * lengthH + radius;  	//Y-Koordinate anhand des Winkels zur gedachten Y-Achse (o(50/50)
+	hourY = radius - (hourY - radius);
+	
+	var c = document.getElementById("clock");
+	var ctx = c.getContext("2d");
+	
+	ctx.canvas.width = radius * 2;//window.innerWidth;
+	ctx.canvas.height = radius * 2;
+	
+	ctx.clearRect(0, 0, c.width, c.height);			//Clear Canvas
+	
+	//Zeichne Ziffernblatt
+	
+	/*ctx.beginPath();
+	ctx.arc(radius, radius, radius, 0, 2*Math.PI);
+	ctx.closePath();
+	ctx.stroke();*/
+	var img = document.getElementById("img");
+	ctx.drawImage(img, 0, 0, radius * 2, radius * 2);
+	
+	//Zeichne Zeiger
+	
+	ctx.beginPath();
+	ctx.moveTo(radius, radius);
+	ctx.lineWidth = 10;
+	ctx.lineTo(hourX, hourY);
+	ctx.stroke();
+	ctx.lineWidth = 5;
+	ctx.moveTo(radius, radius);
+	ctx.lineTo(minuteX, minuteY);
+	ctx.stroke();
+	ctx.closePath();
+	ctx.beginPath();
+	ctx.lineWidth = 1;
+	ctx.strokeStyle = "#ff0000";
+	ctx.moveTo(radius, radius);
+	ctx.lineTo(secondX, secondY);
+	ctx.stroke();
+	ctx.closePath();
+	ctx.beginPath();
+	ctx.strokeStyle = "#000000";
+	ctx.moveTo(radius, radius);
+	ctx.lineTo(milliX, milliY);
+	ctx.closePath(),
+	ctx.stroke();
+}
+
+function resize()
+{
+	if(window.innerHeight < window.innerWidth)
+	{	
+		radius = window.innerHeight / 2;
+	}
+	else
+	{
+		radius = window.innerWidth / 2;
+	}
+	
+	return radius;
+}
+
+function toRadians(angle)
+{
+	return (angle * (Math.PI/180));
+}
